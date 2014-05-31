1a7773177471ccf0bdcf14b12176f6c1f49d07a9
Gabriel Isheden <Gabriel.Isheden@gmail.com>
2014-05-31 16:11:48 +0200

    Started Wordspace_Extraction class

--- wordspace_extraction/Wordspace_Extraction.py
+++ wordspace_extraction/Wordspace_Extraction.py
@@ -1,12 +1,34 @@
-'''
-'''
+'''A class for extracting wordspaces from Gavagai.  '''
 
 
 class Wordspace_Extraction:
     def __init__(self):
+        '''
+        '''
         
         self.Wordspace = None
     
     def check_wordlist():
+        '''Checks if the words in a wordlist is in the wordspace file.  
+        '''
+        
         self.Wordspace = None
-    
+    def extract_wordlist():
+        '''Extracts words from a given wordlist to a wordspace_file.
+        '''
+        
+    def combine():
+        '''Combines several extracted words into a single file.
+        '''
+        
+    def extract_words():
+        '''A generic function for extracting words.
+        '''
+        
+    def extract_full():
+        '''Extracts the full wordspace file loaded into the 
+        Wordspace_Extraction.  '''
+        
+    def save_wordspace():
+        '''Saves all extracted words in a numpy array.
+        '''
