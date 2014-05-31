eb69f445d9aa25b72f8a2d7e052cb381a954811c
Ariel Ekgren <ariel.ekgren@gmail.com>
2014-05-31 16:24:10 +0200

    Changes

--- wordspace_extraction/Wordspace_Extraction.py
+++ wordspace_extraction/Wordspace_Extraction.py
@@ -7,28 +7,37 @@ class Wordspace_Extraction:
         '''
         
         self.Wordspace = None
+        self.vocabulary_set = set()
+
+    def extract_vocabulary(self, path):
+        ''' Extracts vocabulary from word space file.
+        '''
+        with open('C:/Alpha/wordspaces/gavagai-aclice_adventures.wordspace') as f:
+            for line in f:
+                s = line.split(" ")
+                self.vocabulary_set.add(s[0])
     
-    def check_wordlist():
+    def check_wordlist(self):
         '''Checks if the words in a wordlist is in the wordspace file.  
         '''
-        
-        self.Wordspace = None
-    def extract_wordlist():
+
+
+    def extract_wordlist(self):
         '''Extracts words from a given wordlist to a wordspace_file.
         '''
         
-    def combine():
+    def combine(self):
         '''Combines several extracted words into a single file.
         '''
         
-    def extract_words():
+    def extract_words(self):
         '''A generic function for extracting words.
         '''
         
-    def extract_full():
+    def extract_full(self):
         '''Extracts the full wordspace file loaded into the 
         Wordspace_Extraction.  '''
         
-    def save_wordspace():
+    def save_wordspace(self):
         '''Saves all extracted words in a numpy array.
         '''
